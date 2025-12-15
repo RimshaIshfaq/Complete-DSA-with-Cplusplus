bc74fbdabc7eede9043a47862c3894b269b76759
Rimsha Ishfaq <rimshaishfaq86@gmail.com>
2025-12-15 15:47:00 +0500

    Brute Force Approach

---
+++ Algorithms/BruteForceApproach.cpp
@@ -0,0 +1,51 @@
+// first understand the basics of subarray
+// subarray is continuous part of a bigger array
+// subarray = [n* (n+1)]/2
+
+// print all of our sub arrays
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n = 5; 
+    int arr[5] = {1,2,3,4,5};
+    for (int st = 0; st < n; st++){
+        for (int end = 0; end < n-1; end++){
+            for (int i=st; i<=end; i++){
+                cout << arr[i];
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// Maximum Subarrays sum 
+// Brute Force Approach
+
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main(){
+    int n = 5;
+    int arr[5] ={1,2,3,4,5};
+    
+    int maxSum = INT_MIN;
+
+    for(int st = 0; st < n; st++){
+        int currSum = 0;
+        for(int end=st; end<n; end++) {
+            currSum += arr[end];
+            maxSum = max(currSum, maxSum);
+        }
+    }
+    cout << "max subarray sum = " << maxSum << endl;
+
+    return 0;
+}
